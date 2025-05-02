0a6cf63cde2acbe99e21dfb191a4732b1a8af68d
GuiIfanger <guilhermeluizrodriguesifanger@gmail.com>
2025-05-02 19:59:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ app.get('/data-hora', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`); // <-- Isso deve aparecer nos logs
 });
-
-fetch('https://ativ8-api.onrender.com/data-hora')
-  .then(res => res.json())
-  .then(data => console.log(data));
